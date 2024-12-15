37fc31958a022d37f1fbd8325ba45b8fe1bc7dbb
Taylor Bergquist <knexer@gmail.com>
2024-12-15 14:03:54 -0800

    Now is a broken haskell project instead of not a haskell project

---
+++ app/Main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import d11.part_1 (main)
+
+main :: IO ()
+main = do
+  printY2019Day01Part1 -- 3353880
+  printY2019Day01Part2 -- 5027950
+  printY2019Day02Part1 -- 4462686
+  printY2024Day01Part1 -- 2375403
+  printY2024Day01Part2 -- 23082277
+  printY2024Day02Part1 -- 624
+  printY2024Day02Part2 -- 658
+  printY2024Day03Part1 -- 165225049
+  printY2024Day03Part2 -- 108830766
+  printY2024Day04Part1 -- 2414
+  printY2024Day04Part2 -- 1871
