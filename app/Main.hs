f26c7d603e8664f5d52dd0fa6e023a139b6b6646
Taylor Bergquist <knexer@gmail.com>
2024-12-16 20:56:30 -0800

    Too slow naive part 2 solution

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,12 @@ module Main where
 import System.Environment (getArgs)
 import System.FilePath ((</>))
 
-import D11.Part_1
-import D11.Part_2
-import D12.Part_1
-import D12.Part_2
-import D13.Part_1
+import D11.Part_1 (main)
+import D11.Part_2 (main)
+import D12.Part_1 (main)
+import D12.Part_2 (main)
+import D13.Part_1 (main)
+import D13.Part_2 (main)
 
 exes :: Int -> Int -> String -> IO ()
 exes day part
@@ -16,6 +17,7 @@ exes day part
   | day == 12 && part == 1 = D12.Part_1.main
   | day == 12 && part == 2 = D12.Part_2.main
   | day == 13 && part == 1 = D13.Part_1.main
+  | day == 13 && part == 2 = D13.Part_2.main
   | otherwise = undefined
 
 main :: IO ()
---
+++ src/D13/Part_2.hs
@@ -0,0 +1,73 @@
+module D13.Part_2 where
+
+import Data.Char (isDigit)
+import Data.Maybe (catMaybes)
+
+type Vector = (Int, Int)
+
+data Machine = Machine
+  { a :: Vector,
+    b :: Vector,
+    goal :: Vector
+  }
+  deriving (Show)
+
+split :: (Eq a) => a -> [a] -> [[a]]
+split _ [] = []
+split delimiter x = as : split delimiter (drop 1 bs)
+  where
+    (as, bs) = span (/= delimiter) x
+
+extractNumbers :: String -> [Int]
+extractNumbers [] = []
+extractNumbers string
+  | length numbers == 0 = []
+  | otherwise = read numbers : extractNumbers rest
+  where
+    (numbers, rest) = span isDigit (dropWhile (not . isDigit) string)
+
+parseVector :: String -> Vector
+parseVector = makeVector . extractNumbers
+  where
+    makeVector (a : b : _) = (a, b)
+    makeVector _ = undefined
+
+parseMachine :: [String] -> Machine
+parseMachine strings =
+  Machine
+    { a = parseVector (strings !! 0),
+      b = parseVector (strings !! 1),
+      goal = (goalMod + fst origGoal, goalMod + snd origGoal)
+    }
+  where
+    origGoal = parseVector (strings !! 2)
+    goalMod = 10000000000000
+
+pressB :: Machine -> Int -> Maybe Int
+pressB machine aPresses
+  | x == (fst . goal) machine && y == (snd . goal) machine = Just bPresses
+  | otherwise = Nothing
+  where
+    remainingGoal = (fst . goal) machine - aPresses * (fst . a) machine
+    bPresses = remainingGoal `div` ((fst . b) machine)
+    x = aPresses * (fst . a) machine + bPresses * (fst . b) machine
+    y = aPresses * (snd . a) machine + bPresses * (snd . b) machine
+
+minTokens :: Machine -> Maybe Int
+minTokens machine
+  | length justTokens > 0 = Just (foldr1 min justTokens)
+  | otherwise = Nothing
+  where
+    allPresses = map (pressB machine) [0 .. maxA]
+    justTokens = (catMaybes . map tokens) (zip [0 .. maxA] allPresses)
+    maxA = (fst . goal) machine `div` ((fst . a) machine)
+
+tokens :: (Int, Maybe Int) -> Maybe Int
+tokens (_, Nothing) = Nothing
+tokens (a, (Just b)) = Just (3 * a + b)
+
+parse :: String -> [Machine]
+parse input = map parseMachine (((split "") . lines) input)
+
+main :: String -> IO ()
+main = print . sum . catMaybes . (map minTokens) . parse
